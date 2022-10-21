05351f6003c13dc7da8a8c6baf25b8dba4fedf2d
Ryu Injoon <yij6514@iastate.edu>
2022-10-21 12:13:12 -0500

    init

---
+++ discord/src/discord/MyListener.java
@@ -0,0 +1,22 @@
+package discord;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MyListener extends ListenerAdapter{
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		if(event.getAuthor().isBot()) return;
+		
+		Message message = event.getMessage();
+		String content = message.getContentRaw();
+		
+		if(content.equals(Testingbot.prefix + "ping")) {
+			MessageChannel channel = event.getChannel();
+			channel.sendMessage("Pong!").queue();
+		}
+	}
+}
