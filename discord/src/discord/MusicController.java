e3dfb9a40669466e6a7fb642b77e60a9d144c352
Ryu Injoon <yij6514@iastate.edu>
2022-11-01 22:55:54 -0500

    first bot

---
+++ discord/src/discord/MusicController.java
@@ -0,0 +1,12 @@
+package discord;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MusicController extends ListenerAdapter{
+	
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		
+	}
+	
+}
